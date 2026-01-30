5098ca27715795206802752d88046a79e81f7a06
Aaron Joseph Kantsevoy <akantsevoy3@rg-login.crnch.gatech.edu>
2026-01-30 09:41:05 -0500

    refactor and test update

--- ci/run-crnch-cuda-test.sh
+++ ci/run-crnch-cuda-test.sh
@@ -8,12 +8,8 @@
 #SBATCH -o /tools/ci-reports/CuPBoP_logs/CuPBoP-cuda-test-%j.out   # Combined output and error messages file
 #SBATCH -W                                       # Do not exit until the submitted job terminates.
 
-##Add commands here to build and execute
 cd $GITHUB_WORKSPACE
 hostname
-
-#module use /projects/tools/x86_64/ubuntu-22.04/nvhpc-24.9/modulefiles/
-#module load nvhpc/24.9
 
 export FINAL_OUTPUT_PATH="/tools/ci-reports/CuPBoP_logs/CuPBoP-cuda-test-${SLURM_JOB_ID}.out"
 apptainer exec --nv -B /projects/ci-runners/CuPBoP-Vortex/:/projects/ci-runners/CuPBoP-Vortex/ /projects/ci-runners/CuPBoP-Vortex/tools/cupbop_env.sif /bin/bash << 'EOF'
--- ci/run-crnch-cuda.sh
+++ ci/run-crnch-cuda.sh
@@ -2,13 +2,12 @@
 #SBATCH --job-name="CuPBoP-Vortex-ci-cuda"
 #SBATCH -t 00:10:00                              # Duration of the job
 #SBATCH -p rg-nextgen-hpc
-#SBATCH -w dash3
+#SBATCH -w dash[1-4]
 #SBATCH -N 1
 #SBATCH --gres=gpu:1
 #SBATCH -o /tools/ci-reports/CuPBoP_logs/CuPBoP-cuda-test-%j.out   # Combined output and error messages file
 #SBATCH -W                                       # Do not exit until the submitted job terminates.
 
-##Add commands here to build and execute
 cd $GITHUB_WORKSPACE
 hostname
 
@@ -16,16 +15,6 @@ rm -rf build
 
 apptainer exec --nv -B /projects/ci-runners/CuPBoP-Vortex/:/projects/ci-runners/CuPBoP-Vortex/ /projects/ci-runners/CuPBoP-Vortex/tools/cupbop_env.sif /bin/bash << 'EOF'
     source "./ci/rg-ci-setup.sh"
-    #export CuPBoP_PATH=$PWD
-    #bash $CuPBoP_PATH/ci/toolchain_install.sh --vortex
-
-    # Installing prebuilt Vortex Toolchain
-    # cd vortex
-    # mkdir tools
-    # export TOOLDIR=$CuPBoP_PATH/vortex/tools # <== make tooldir visible to child processes (i.e. bash scripts)
-    # bash $CuPBoP_PATH/ci/toolchain_install.sh --all #NOTE <-- this will have to be changed to use the correct version of LLVM
-    # cd build
-    # ../configure --xlen=64 --tooldir=$TOOLDIR
 
     # Installing CUDA headers for CuPBoP
     cd $CuPBoP_PATH
@@ -35,7 +24,6 @@ apptainer exec --nv -B /projects/ci-runners/CuPBoP-Vortex/:/projects/ci-runners/
     rm -r 'cuda-header.tar.gz'
 
     mkdir -p $CuPBoP_PATH/build && cd $CuPBoP_PATH/build
-    # export LLVM_CONFIG_PATH=$LLVM_VORTEX/bin/llvm-config
     cmake .. -DLLVM_CONFIG_PATH=$LLVM_CONFIG_PATH
     make
 EOF
